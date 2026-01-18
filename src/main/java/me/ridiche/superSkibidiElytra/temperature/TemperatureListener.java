c2ab54df77c8686fdefd4c7ee11b16a3c114f95c
ridiche <tudo.diac@gmail.com>
2026-01-18 19:52:55 +0200

    Added custom death message to dying to particles

--- src/main/java/me/ridiche/superSkibidiElytra/temperature/TemperatureListener.java
+++ src/main/java/me/ridiche/superSkibidiElytra/temperature/TemperatureListener.java
@@ -7,20 +7,25 @@ import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.Particle;
+import org.bukkit.World;
 import org.bukkit.damage.DamageSource;
 import org.bukkit.damage.DamageType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.util.Vector;
 
 public class TemperatureListener implements Listener {
+    private static boolean isHot(World world) {
+        return world.getKey().equals(new NamespacedKey("minecraft", "the_nether"));
+    }
+
     @EventHandler
     public static void onTick(ServerTickEndEvent event) {
         for(Player plr : Bukkit.getOnlinePlayers()) {
-            if (plr.isGliding()
-                && plr.getWorld().getKey().equals(new NamespacedKey("minecraft", "the_nether"))) {
+            if (plr.isGliding() && isHot(plr.getWorld())) {
                 if (plr.getVelocity().lengthSquared() > 0.5f) {
                     // Hurt
                     plr.damage(2f, DamageSource.builder(DamageType.IN_FIRE).build());
@@ -41,6 +46,19 @@ public class TemperatureListener implements Listener {
                     );
                 }
             }
+        }
+    }
+
+    @EventHandler
+    public static void onPlayerDeath(PlayerDeathEvent event) {
+        if (event.getDamageSource().getDamageType() == DamageType.IN_FIRE
+                && event.getPlayer().isGliding()
+                && isHot(event.getPlayer().getWorld())) {
+            String whoDied = event.getPlayer().getName();
+            event.deathMessage(
+                    Component.text(
+                            whoDied+ " flew too close to the sun, except that the sun is lava, or wait, actually... The particles are from the enviornment, they're in the other biomes too... Wait, no, "+whoDied+" flew too close to the... flying, uh... hot micro-suns..? You know what, maybe this isn't a good metaphor after all."
+                    ));
         }
     }
 }
